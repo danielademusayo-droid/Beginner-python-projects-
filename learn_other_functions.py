2cdfb931da8243f76ed3a21692a0507311d87862
Daniel Ademusayo <danielademusayo@gmail.com>
2026-08-05 10:12:51 +0100

    Add learn_other_functions.py file

--- learn_other_functions.py
+++ learn_other_functions.py
@@ -1,10 +1,10 @@
-#age = int(input("Enter your age: "))	
-
-#print("You are anAdult" if age >= 18  else "You are a child")
 name = input("Enter your full name: ")
+age = int(input("Enter your age: "))	
 
-#result = len(name)
+result = len(name)
 result =  name.find("'d")
+
+print("You are anAdult" if age >= 18  else "You are a child")
 
 print(result)
 #print(help(str)) to check other functiin that i might need
