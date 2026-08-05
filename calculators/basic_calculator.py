a70802ee2f95ff38ec8b9a1866c04110b34f6896
Daniel Ademusayo <danielademusayo@gmail.com>
2026-08-05 10:27:06 +0100

    Implement basic calculator functionality

--- calculators/basic_calculator.py
+++ calculators/basic_calculator.py
@@ -1,1 +1,20 @@
+print("---Basic Calculator---")
+# Project to build my problem solving and critical thinking skills
+num1= float(input("First Number: "))
+num2= float(input("Second Number: "))
+Operator = input("Enter the operator (+,-,*,/): ")
+
+if Operator == "+":
+	result = num1 + num2
+elif Operator == "-":
+	  result = num1 - num2
+elif Operator == "*":
+	  round(result = num1 * num2),2
+elif Operator == "/":
+	  result = num1 / num2  if num2 != 0 else "Cannot divide by zero!"
+else: 
+      result = "invalid operation!"
+      
+print(f"Result: {result}")
+
 
